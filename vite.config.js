36fb2d89b0a24e006865b7af29edc2c5d087491e
Fengying Zhang <fengyingzhang206@gmail.com>
2024-10-15 20:08:07 +1100

    change config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,13 +11,12 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  base: process.env.NODE_ENV === 'production'
     ? '/fit5032-week5/'
-    : '/'
-}
+    : '/',
+});
+
