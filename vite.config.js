64c2b9a87f5776b8527e6e6ae2cdd8bb49647587
Fengying Zhang <fengyingzhang206@gmail.com>
2024-10-15 20:19:53 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
-
+// test deployment
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
